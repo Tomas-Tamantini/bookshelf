ac8b6cb9e399508d4b1fab07821294787443136e
Tomas Tamantini <tomasbft@gmail.com>
2024-07-07 19:32:01 -0300

    Create client test fixture

--- tests/test_api/test_author.py
+++ tests/test_api/test_author.py
@@ -1,12 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from bookshelf.app import app
-
-
-def test_creating_valid_author_returns_status_ok():
-    client = TestClient(app)
+def test_creating_valid_author_returns_status_ok(client):
     response = client.post(
         "/authors",
         json={"name": "Andrew Hunt"},
--- tests/test_api/test_health_check.py
+++ tests/test_api/test_health_check.py
@@ -1,11 +1,6 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from bookshelf.app import app
-
-
-def test_health_check_returns_no_content():
-    client = TestClient(app)
+def test_health_check_returns_no_content(client):
     response = client.get("/health")
     assert response.status_code == HTTPStatus.NO_CONTENT
