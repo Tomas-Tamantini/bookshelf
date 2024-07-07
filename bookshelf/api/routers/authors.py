19618a0421cde9de5813c023a7bfb3c9f474d053
Tomas Tamantini <tomasbft@gmail.com>
2024-07-07 19:48:47 -0300

    Implement CreateAuthorRequest schema

--- bookshelf/api/routers/authors.py
+++ bookshelf/api/routers/authors.py
@@ -2,9 +2,11 @@ from http import HTTPStatus
 
 from fastapi import APIRouter
 
+from bookshelf.api.dto import CreateAuthorRequest
+
 authors_router = APIRouter(prefix="/authors", tags=["authors"])
 
 
 @authors_router.post("/", status_code=HTTPStatus.OK.value)
-def create_author():
+def create_author(author: CreateAuthorRequest):
     return {}
--- tests/test_api/test_author.py
+++ tests/test_api/test_author.py
@@ -1,9 +1,11 @@
 from http import HTTPStatus
 
 
+def test_creating_author_with_missing_fields_returns_unprocessable_entity(client):
+    response = client.post("/authors", json={})
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
 def test_creating_valid_author_returns_status_ok(client):
-    response = client.post(
-        "/authors",
-        json={"name": "Andrew Hunt"},
-    )
+    response = client.post("/authors", json={"name": "Andrew Hunt"})
     assert response.status_code == HTTPStatus.OK
