d651c2ac4a29373848b7746615fdb9da733fe3d8
Tomas Tamantini <tomasbft@gmail.com>
2024-07-07 22:18:07 -0300

    fix: Persist in-memory author repository

--- bookshelf/api/dependencies/get_repositories.py
+++ bookshelf/api/dependencies/get_repositories.py
@@ -5,9 +5,11 @@ from fastapi import Depends
 from bookshelf.repositories.in_memory import InMemoryAuthorRepository
 from bookshelf.repositories.protocols import AuthorRepository
 
+in_memory_repo = InMemoryAuthorRepository()
+
 
 def get_author_repository() -> AuthorRepository:
-    return InMemoryAuthorRepository()
+    return in_memory_repo
 
 
 T_AuthorRepository = Annotated[AuthorRepository, Depends(get_author_repository)]
