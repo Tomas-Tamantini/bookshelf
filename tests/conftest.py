6d24416a53145a52478d8c3427aff436faf2f511
Tomas Tamantini <tomasbft@gmail.com>
2024-07-07 21:54:16 -0300

    Create author fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,3 @@
+from tests.fixtures.author import author
 from tests.fixtures.author_repository import author_repo_mock
 from tests.fixtures.client import client
--- tests/fixtures/author_repository.py
+++ tests/fixtures/author_repository.py
@@ -6,5 +6,7 @@ from bookshelf.repositories.protocols import AuthorRepository
 
 
 @pytest.fixture
-def author_repo_mock():
-    return Mock(spec=AuthorRepository)
+def author_repo_mock(author):
+    repo = Mock(spec=AuthorRepository)
+    repo.add.return_value = author
+    return repo
