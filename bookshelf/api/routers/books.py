46561d24a533bd94581f99a7b411756dd3f3ecf4
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-09 13:47:08 -0300

    Store new book in book repository

--- bookshelf/api/routers/books.py
+++ bookshelf/api/routers/books.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 from fastapi import APIRouter
 
+from bookshelf.api.dependencies import T_BookRepository
 from bookshelf.api.dto import CreateBookRequest
 from bookshelf.domain.book import Book
 
@@ -9,5 +10,5 @@ books_router = APIRouter(prefix="/books", tags=["books"])
 
 
 @books_router.post("/", status_code=HTTPStatus.CREATED.value, response_model=Book)
-def create_book(book: CreateBookRequest):
-    pass
+def create_book(book: CreateBookRequest, book_repository: T_BookRepository):
+    return book_repository.add(book)
--- tests/test_api/test_book.py
+++ tests/test_api/test_book.py
@@ -4,3 +4,25 @@ from http import HTTPStatus
 def test_creating_book_with_missing_fields_returns_unprocessable_entity(client):
     response = client.post("/books", json={})
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+def test_creating_valid_book_returns_status_created(client):
+    response = client.post(
+        "/books",
+        json={"title": "The Pragmatic Programmer", "year": 1999, "author_id": 1},
+    )
+    assert response.status_code == HTTPStatus.CREATED
+
+
+def test_creating_valid_book_returns_book_with_id_given_by_repository(
+    client, mock_book_repository
+):
+    stored_book = {"id": 123, "title": "Stored title", "year": 1999, "author_id": 1}
+
+    mock_book_repository.add.return_value = stored_book
+
+    response = client.post(
+        "/books",
+        json={"title": "The Pragmatic Programmer", "year": 1999, "author_id": 1},
+    )
+    assert response.json() == stored_book
