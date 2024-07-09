3bb7eff7cce0992b393d732dee230c4de0ee78ce
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-09 14:09:32 -0300

    Extract valid book request to fixture

--- tests/fixtures/book.py
+++ tests/fixtures/book.py
@@ -6,3 +6,8 @@ from bookshelf.domain.book import Book
 @pytest.fixture
 def book():
     return Book(id=1, title="Book Title", author_id=1, year=2021)
+
+
+@pytest.fixture
+def valid_book_request():
+    return {"title": "The Pragmatic Programmer", "year": 1999, "author_id": 1}
--- tests/test_api/test_book.py
+++ tests/test_api/test_book.py
@@ -6,25 +6,19 @@ def test_creating_book_with_missing_fields_returns_unprocessable_entity(client):
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
 
-def test_creating_valid_book_returns_status_created(client):
-    response = client.post(
-        "/books",
-        json={"title": "The Pragmatic Programmer", "year": 1999, "author_id": 1},
-    )
+def test_creating_valid_book_returns_status_created(client, valid_book_request):
+    response = client.post("/books", json=valid_book_request)
     assert response.status_code == HTTPStatus.CREATED
 
 
 def test_creating_valid_book_returns_book_with_id_given_by_repository(
-    client, mock_book_repository
+    client, mock_book_repository, valid_book_request
 ):
     stored_book = {"id": 123, "title": "Stored title", "year": 1999, "author_id": 1}
 
     mock_book_repository.add.return_value = stored_book
 
-    response = client.post(
-        "/books",
-        json={"title": "The Pragmatic Programmer", "year": 1999, "author_id": 1},
-    )
+    response = client.post("/books", json=valid_book_request)
     assert response.json() == stored_book
 
 
@@ -37,25 +31,19 @@ def test_book_name_gets_sanitized_beofre_being_stored(client, mock_book_reposito
 
 
 def test_creating_book_with_existing_title_returns_conflict(
-    client, mock_book_repository
+    client, mock_book_repository, valid_book_request
 ):
     mock_book_repository.title_exists.return_value = True
 
-    response = client.post(
-        "/books",
-        json={"title": "The Pragmatic Programmer", "year": 1999, "author_id": 1},
-    )
+    response = client.post("/books", json=valid_book_request)
     assert response.status_code == HTTPStatus.CONFLICT
     assert response.json() == {"detail": "Book with this title already exists"}
 
 
 def test_creating_book_with_ineixsting_author_returns_not_found(
-    client, mock_author_repository
+    client, mock_author_repository, valid_book_request
 ):
     mock_author_repository.id_exists.return_value = False
 
-    response = client.post(
-        "/books",
-        json={"title": "The Pragmatic Programmer", "year": 1999, "author_id": 1},
-    )
+    response = client.post("/books", json=valid_book_request)
     assert response.status_code == HTTPStatus.NOT_FOUND
