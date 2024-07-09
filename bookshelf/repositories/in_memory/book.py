965d6c4c7dec4f2ec00eb9fdf5465179dbc9aac1
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-09 14:22:34 -0300

    Start implementing in-memory book repository

--- bookshelf/repositories/in_memory/book.py
+++ bookshelf/repositories/in_memory/book.py
@@ -2,8 +2,13 @@ from bookshelf.domain.book import Book, BookCore
 
 
 class InMemoryBookRepository:
+    def __init__(self) -> None:
+        self._books = []
+
     def add(self, book: BookCore) -> Book:
-        raise NotImplementedError()
+        book = Book(id=len(self._books) + 1, **book.model_dump())
+        self._books.append(book)
+        return book
 
     def title_exists(self, title: str) -> bool:
-        raise NotImplementedError()
+        return any(book.title == title for book in self._books)
---
+++ tests/test_persistence/test_in_memory_persistence/test_in_memory_books_repository.py
@@ -0,0 +1,22 @@
+import pytest
+
+from bookshelf.domain.book import Book, BookCore
+from bookshelf.repositories.in_memory import InMemoryBookRepository
+
+
+@pytest.fixture
+def repository():
+    return InMemoryBookRepository()
+
+
+def test_adding_book_in_memory_increments_id(repository):
+    book_1 = repository.add(BookCore(title="Book 1", author_id=1, year=2021))
+    assert book_1 == Book(id=1, title="Book 1", author_id=1, year=2021)
+    book_2 = repository.add(BookCore(title="Book 2", author_id=2, year=2021))
+    assert book_2 == Book(id=2, title="Book 2", author_id=2, year=2021)
+
+
+def test_in_memory_book_repository_keeps_track_of_existing_titles(repository):
+    repository.add(BookCore(title="Book 1", author_id=1, year=2021))
+    assert repository.title_exists("Book 1")
+    assert not repository.title_exists("Book 2")
