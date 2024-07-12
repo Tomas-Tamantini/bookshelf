cfd35d97a3ca6db6dcb67c024830905f8f6e2d04
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-07-12 15:20:40 -0300

    Define pagination parameters

--- bookshelf/repositories/dto/pagination.py
+++ bookshelf/repositories/dto/pagination.py
@@ -2,6 +2,12 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
+class PaginationParameters:
+    limit: int
+    offset: int
+
+
+@dataclass(frozen=True)
 class RepositoryPaginatedResponse[T]:
     elements: list[T]
     total: int
