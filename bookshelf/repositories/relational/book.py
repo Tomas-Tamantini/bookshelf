fb88d66891229913403a19d5b603d2c9e9fa8363
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-08-08 10:38:15 -0300

    Extract base relational repository class

--- bookshelf/repositories/relational/book.py
+++ bookshelf/repositories/relational/book.py
@@ -1,22 +1,14 @@
-from typing import Optional
-
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Query
 
 from bookshelf.domain.book import Book, BookCore
-from bookshelf.repositories.dto import (
-    BookFilters,
-    PaginationParameters,
-    RepositoryPaginatedResponse,
-)
-from bookshelf.repositories.exceptions import ConflictError
+from bookshelf.repositories.dto import BookFilters
+from bookshelf.repositories.relational.relational_repository import RelationalRepository
 from bookshelf.repositories.relational.tables import BookDB
 
 
-class RelationalBookRepository:
-    def __init__(self, session: Session) -> None:
-        self._session = session
-
+class RelationalBookRepository(
+    RelationalRepository[BookCore, Book, BookDB, BookFilters]
+):
     @staticmethod
     def _db_to_domain(db_element: BookDB) -> Book:
         return Book(
@@ -27,55 +19,18 @@ class RelationalBookRepository:
         )
 
     @staticmethod
-    def _raise_conflict_error(e: IntegrityError) -> None:
-        field = str(e.orig).split(".")[-1].strip()
-        raise ConflictError(field)
+    def _domain_to_db(domain_element: BookCore) -> BookDB:
+        return BookDB(**domain_element.model_dump())
 
-    def add(self, element: BookCore) -> Book:
-        book_db = BookDB(**element.model_dump())
-        self._session.add(book_db)
-        try:
-            self._session.commit()
-            self._session.refresh(book_db)
-            return self._db_to_domain(book_db)
-        except IntegrityError as e:
-            self._session.rollback()
-            self._raise_conflict_error(e)
+    @staticmethod
+    def _db_class() -> BookDB:
+        return BookDB
 
-    def id_exists(self, id: int) -> bool:
-        return self._session.query(BookDB).filter_by(id=id).count() > 0
-
-    def delete(self, element_id: int) -> None:
-        self._session.query(BookDB).filter_by(id=element_id).delete()
-        self._session.commit()
-
-    def update(self, element_id: int, element: BookCore) -> Book:
-        try:
-            self._session.query(BookDB).filter(BookDB.id == element_id).update(
-                element.model_dump()
-            )
-            self._session.commit()
-            book_db = self._session.query(BookDB).filter_by(id=element_id).one()
-            return self._db_to_domain(book_db)
-        except IntegrityError as e:
-            self._session.rollback()
-            self._raise_conflict_error(e)
-
-    def get_by_id(self, element_id: int) -> Optional[Book]:
-        book_db = self._session.query(BookDB).filter_by(id=element_id).one_or_none()
-        return self._db_to_domain(book_db) if book_db else None
-
-    def get_filtered(
-        self, pagination: PaginationParameters, filters: BookFilters
-    ) -> RepositoryPaginatedResponse[Book]:
-        query = self._session.query(BookDB)
+    def _apply_filters(self, query: Query, filters: BookFilters) -> Query:
         if filters.title:
             query = query.where(BookDB.title.contains(filters.title))
         if filters.year:
             query = query.where(BookDB.year == filters.year)
         if filters.author_id:
             query = query.where(BookDB.author_id == filters.author_id)
-        total = query.count()
-        query = query.limit(pagination.limit).offset(pagination.offset)
-        books = [self._db_to_domain(book) for book in query.all()]
-        return RepositoryPaginatedResponse(elements=books, total=total)
+        return query
---
+++ bookshelf/repositories/relational/relational_repository.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Query, Session
+
+from bookshelf.repositories.dto import PaginationParameters, RepositoryPaginatedResponse
+from bookshelf.repositories.exceptions import ConflictError
+
+
+class RelationalRepository[Element, ElementWithId, ElementDB, Filters](ABC):
+    def __init__(self, session: Session) -> None:
+        self._session = session
+
+    @abstractmethod
+    def _db_to_domain(db_element: ElementDB) -> ElementWithId:
+        raise NotImplementedError("Abstract method")
+
+    @abstractmethod
+    def _domain_to_db(domain_element: Element) -> ElementDB:
+        raise NotImplementedError("Abstract method")
+
+    @abstractmethod
+    def _db_class(self) -> ElementDB:
+        raise NotImplementedError("Abstract method")
+
+    @abstractmethod
+    def _apply_filters(self, query, filters: Filters):
+        raise NotImplementedError("Abstract method")
+
+    def _query_table(self) -> Query:
+        return self._session.query(self._db_class())
+
+    @staticmethod
+    def _raise_conflict_error(e: IntegrityError) -> None:
+        field = str(e.orig).split(".")[-1].strip()
+        raise ConflictError(field)
+
+    def add(self, element: Element) -> ElementWithId:
+        element_db = self._domain_to_db(element)
+        self._session.add(element_db)
+        try:
+            self._session.commit()
+            self._session.refresh(element_db)
+            return self._db_to_domain(element_db)
+        except IntegrityError as e:
+            self._session.rollback()
+            self._raise_conflict_error(e)
+
+    def id_exists(self, id: int) -> bool:
+        return self._query_table().filter_by(id=id).count() > 0
+
+    def delete(self, element_id: int) -> None:
+        self._query_table().filter_by(id=element_id).delete()
+        self._session.commit()
+
+    def get_by_id(self, element_id: int) -> Optional[ElementWithId]:
+        element_db = self._query_table().filter_by(id=element_id).one_or_none()
+        return self._db_to_domain(element_db) if element_db else None
+
+    def get_filtered(
+        self, pagination: PaginationParameters, filters: Filters
+    ) -> RepositoryPaginatedResponse[ElementWithId]:
+        query = self._query_table()
+        query = self._apply_filters(query, filters)
+        total = query.count()
+        query = query.limit(pagination.limit).offset(pagination.offset)
+        elements = [self._db_to_domain(element) for element in query.all()]
+        return RepositoryPaginatedResponse(elements, total=total)
+
+    def update(self, element_id: int, element: Element) -> ElementWithId:
+        try:
+            self._query_table().filter(self._db_class().id == element_id).update(
+                element.model_dump()
+            )
+            self._session.commit()
+            element_db = self._query_table().filter_by(id=element_id).one()
+            return self._db_to_domain(element_db)
+        except IntegrityError as e:
+            self._session.rollback()
+            self._raise_conflict_error(e)
